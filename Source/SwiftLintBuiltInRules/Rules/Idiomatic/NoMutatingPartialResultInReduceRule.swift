fcb7e5bec44ac1166bf8d55c98b1e44e9cdafe0f
Alfons Hoogervorst <alfons.hoogervorst@gmail.com>
2025-05-14 06:20:31 +0200

    Refactor a bit

--- Source/SwiftLintBuiltInRules/Rules/Idiomatic/NoMutatingPartialResultInReduceRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Idiomatic/NoMutatingPartialResultInReduceRule.swift
@@ -20,6 +20,13 @@ struct NoMutatingPartialResultInReduceRule: Rule {
                     return partialResult
                 }
             """),
+            Example("""
+                let value = input.reduce(0, { accum, inc in
+                    ↓var accum = accum
+                    accum = accum + 1
+                    return accum
+                }
+            """)
         ]
     )
 }
@@ -27,73 +34,110 @@ struct NoMutatingPartialResultInReduceRule: Rule {
 private extension NoMutatingPartialResultInReduceRule {
     final class Visitor: ViolationsSyntaxVisitor<ConfigurationType> {
         
+        /// Looking for:
+        /// * a function call `reduce`,
+        /// * with a trailing closure,
+        /// * the closure's first argument (the accumulator)
+        /// * get the variable declarations that reference
+        ///   the accumulator,
         override func visitPost(_ node: FunctionCallExprSyntax) {
-            // should be some reduce()
-            guard let memberAccessExprSyntax = node.calledExpression.as(MemberAccessExprSyntax.self),
-                  memberAccessExprSyntax.declName.baseName.text == "reduce" else {
-                return
-            }
-            guard node.arguments.count == 2 else {
-                // we should have two arguments to reduce
-                return
-            }
-            guard node.arguments.kind == .labeledExprList,
-                  node.arguments.first?.label?.text != "into" else {
-                // it's a `reduce(into:_)`
-                return
-            }
-            guard let closureExpr = node.arguments.last?.expression.as(ClosureExprSyntax.self),
-                  let parameterList = closureExpr.signature?.parameterClause?.as(ClosureShorthandParameterListSyntax.self),
-                  let partialResultVarName = parameterList.first?.name.text else {
+            guard node.isReduceFunctionCall() else { return }
+            guard let partialResultVarName = node.reducePartialResultVariableName() else {
                 // could not get the var name of the partial result
                 return
             }
-            guard let variableDecls = closureExpr.statements.findVariablesReferencing(partialResultVarName) else {
-                // nothing found
+            guard let variables = node.trailingClosure()?.variablesReferencing(partialResultVarName) else {
+                // no variables referencing partial result variable
                 return
             }
-            let absolutePositions = variableDecls
+            let absolutePositions = variables
                 .map(\.positionAfterSkippingLeadingTrivia)
             self.violations.append(contentsOf: absolutePositions)
         }
     }
 }
 
-
-extension CodeBlockItemListSyntax {
+private extension FunctionCallExprSyntax {
     
-    func findVariablesReferencing(_ name: String) -> [VariableDeclSyntax]? {
-        
-        func asVariableDecl(_ syntax: CodeBlockItemSyntax) -> VariableDeclSyntax? {
-            return syntax.item.as(VariableDeclSyntax.self)
+    func isReduceFunctionCall() -> Bool {
+        // should be some reduce()
+        guard let memberAccessExprSyntax = self.calledExpression.as(MemberAccessExprSyntax.self),
+              memberAccessExprSyntax.declName.baseName.text == "reduce" else {
+            return false
         }
-        
-        func isAssignmentToAVariable(_ variableDeclSyntax: VariableDeclSyntax) -> Bool {
-            return variableDeclSyntax.bindingSpecifier.tokenKind == .keyword(.var)
+        guard self.arguments.count == 2 else {
+            // we should have two arguments to reduce
+            return false
         }
-        
-        func isReferencingOurVariable(_ variableDeclSyntax: VariableDeclSyntax) -> Bool {
-            guard let binding = variableDeclSyntax.bindings.first else {
-                // no variable assignment
-                return false
-            }
-            guard binding.pattern.is(IdentifierPatternSyntax.self) else {
-                // no identifier
-                return false
-            }
-            guard let initializer = binding.initializer else {
-                // no assignment
-                return false
-            }
-            guard let rhsValue = initializer.value.as(DeclReferenceExprSyntax.self) else {
-                return false
-            }
-            return rhsValue.baseName.text == name
+        guard self.arguments.kind == .labeledExprList,
+              self.arguments.first?.label?.text != "into" else {
+            // it's a `reduce(into:_)`
+            return false
         }
-        let variableDecls = self.compactMap(asVariableDecl)
-            .filter(isAssignmentToAVariable)
-            .filter(isReferencingOurVariable)
-        return variableDecls.count > 0 ? variableDecls : nil
+        guard self.arguments.last?.expression.as(ClosureExprSyntax.self) != nil else {
+            // no closure
+            return false
+        }
+        return true
     }
     
+    func reducePartialResultVariableName() -> String? {
+        guard let closureExpr = self.arguments.last?.expression.as(ClosureExprSyntax.self),
+              let paramList = closureExpr.signature?.parameterClause?.as(ClosureShorthandParameterListSyntax.self),
+              let partialResultVarName = paramList.first?.name.text else {
+            // could not get the var name of the partial result
+            return nil
+        }
+        return partialResultVarName
+    }
+    
+    func trailingClosure() -> ClosureExprSyntax? {
+        return self.arguments.last?.expression.as(ClosureExprSyntax.self)
+    }
+}
+
+private extension ClosureExprSyntax {
+
+    func variablesReferencing(_ name: String) -> [VariableDeclSyntax]? {
+        return self.statements.variablesReferencing(name)
+    }
+}
+
+private extension CodeBlockItemListSyntax {
+    
+    func variablesReferencing(_ name: String) -> [VariableDeclSyntax]? {
+        let variableDecls = self.compactMap(\.asVariableDecl)
+            .filter(\.isAssignmentToAVariable)
+            .filter(VariableReference(name: name).matches(_:))
+        return !variableDecls.isEmpty ? variableDecls : nil
+    }
+}
+
+private extension CodeBlockItemSyntax {
+
+    var asVariableDecl: VariableDeclSyntax? {
+        return self.item.as(VariableDeclSyntax.self)
+    }
+}
+
+private extension VariableDeclSyntax {
+    
+    var isAssignmentToAVariable: Bool {
+        return self.bindingSpecifier.tokenKind == .keyword(.var)
+    }
+}
+
+private struct VariableReference {
+    let name: String
+    
+    func matches(_ variableDeclSyntax: VariableDeclSyntax) -> Bool {
+        // should be a variable assignment, with identifier and assignment
+        guard let binding = variableDeclSyntax.bindings.first,
+              binding.pattern.is(IdentifierPatternSyntax.self),
+              let initializer = binding.initializer,
+              let rhsValue = initializer.value.as(DeclReferenceExprSyntax.self) else {
+            return false
+        }
+        return rhsValue.baseName.text == name
+    }
 }
